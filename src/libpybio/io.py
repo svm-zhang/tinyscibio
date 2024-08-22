ed16d886b913c4797afe41beaf5614a693c65dbc
simo <svm.zhang@gmail.com>
2024-08-22 14:46:56 -0700

    initial commit; io library dealing with file and dir operations

---
+++ src/libpybio/io.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+PathLike = Path | str
+
+
+def parse_path(path: PathLike) -> Path:
+    """Parse a given path."""
+    if isinstance(path, Path):
+        return path
+    return Path(path)
+
+
+def make_dir(path: PathLike, parents: bool = False, exist_ok: bool = False) -> None:
+    """Make new directory at the given path."""
+    path = parse_path(path)
+    if not path.exists():
+        path.mkdir(parents=parents, exist_ok=exist_ok)
