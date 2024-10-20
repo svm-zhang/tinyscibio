44ece2b2aef54625b456f6e1fd953fd28a1a0005
simo <svm.zhang@gmail.com>
2024-10-19 22:38:54 -0700

    initial commit; documentation site using mkdocs-material

--- src/libscibio/__init__.py
+++ src/libscibio/__init__.py
@@ -2,6 +2,16 @@ from __future__ import annotations
 
 from ._io import _PathLike, get_parent_dir, make_dir, parse_path
 from .api_server import request_api_server
+from .bam import (
+    BAMetadata,
+    count_indel_bases,
+    count_indel_events,
+    count_mismatch_events,
+    count_soft_clip_bases,
+    count_unaligned_events,
+    parse_cigar,
+    parse_md,
+)
 
 __all__ = [
     "_PathLike",
@@ -9,4 +19,11 @@ __all__ = [
     "parse_path",
     "get_parent_dir",
     "request_api_server",
+    "parse_md",
+    "parse_cigar",
+    "count_unaligned_events",
+    "count_indel_events",
+    "count_mismatch_events",
+    "count_soft_clip_bases",
+    "count_indel_bases",
 ]
