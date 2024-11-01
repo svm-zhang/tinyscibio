7bc6f59a4dc127c566c105c911a7f5b5db750d15
simo <svm.zhang@gmail.com>
2024-10-31 23:00:02 -0700

    Added func to read bed to df; simplified _overlap function by just doing one task: find overlaps

--- src/tinyscibio/_intervals.py
+++ src/tinyscibio/_intervals.py
@@ -4,41 +4,54 @@ import numpy as np
 import polars as pl
 from ncls import NCLS
 
+from tinyscibio import _PathLike, parse_path
 
-def find_overlaps(
-    ranges_a: pl.DataFrame, ranges_b: pl.DataFrame
-) -> pl.DataFrame:
+
+def extract_intervals_from_bed(bed_file: _PathLike) -> pl.DataFrame:
+    """
+    Retrieve intervals from given BED file.
+    """
+    bed_file = parse_path(bed_file)
+
+    intervals = pl.read_csv(bed_file, has_header=False, separator="\t")
+    n_cols = intervals.shape[1]
+    if n_cols < 3:
+        raise ValueError("Expect at least 3 columns in a BED file.")
+    return intervals
+
+
+def _overlaps(
+    intervals_a: pl.DataFrame, intervals_b: pl.DataFrame
+) -> tuple[int, int]:
     """
     Find overlapping intervals between two set of ranges.
     """
-    starts = pl.Series(np.array([-100, -200]))
-    ends = starts + 50
-
-    subject_df = pl.DataFrame({"sstart": starts, "send": ends}).with_row_index(
-        "sindex"
-    )
-    print(subject_df)
     ncls = NCLS(
-        ranges_b.get_column("sstart").__array__(),
-        ranges_b.get_column("send").__array__(),
-        ranges_b.get_column("sindex").__array__(dtype=np.int64),
+        intervals_b[:, 1].__array__(),
+        intervals_b[:, 2].__array__(),
+        np.array(range(0, intervals_b.shape[1]), dtype=np.int64),
     )
     print(ncls)
 
-    qstarts = pl.Series(np.array([-52, -180]))
-    qends = pl.Series([-2, -120])
-    query_df = pl.DataFrame({"qstart": qstarts, "qend": qends}).with_row_index(
-        "qindex"
+    return ncls.all_overlaps_both(
+        intervals_a[:, 1].__array__(),
+        intervals_a[:, 2].__array__(),
+        np.array(range(0, intervals_a.shape[1]), dtype=np.int64),
     )
-    print(query_df)
 
-    l_idx, r_idx = ncls.all_overlaps_both(
-        ranges_a.get_column("qstart").__array__(),
-        ranges_a.get_column("qend").__array__(),
-        ranges_a.get_column("qindex").__array__(dtype=np.int64),
+
+if __name__ == "__main__":
+    intervals_a = pl.DataFrame(
+        {"chr": ["chr1", "chr2"], "start": [1, 200], "end": [51, 250]}
     )
+    intervals_b = pl.DataFrame(
+        {"chr": ["chr1", "chr2"], "start": [38, 120], "end": [76, 240]}
+    )
+
+    # TODO: split intervals by chr
+    l_idx, r_idx = _overlaps(intervals_a, intervals_b)
+    # FIXME: check if no overlaps found, return None
+    # FIXME: also need to assert l and r idx have the same length
+    # FIXME: Need to make sure chr are the same between l and r
     print(l_idx)
     print(r_idx)
-    print(subject_df[r_idx])
-    print(query_df[l_idx])
-    return pl.concat([query_df[l_idx], subject_df[r_idx]], how="horizontal")
