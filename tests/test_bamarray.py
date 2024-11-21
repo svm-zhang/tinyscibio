59417cc72c63b566d184ef3a44d2facd00187e1c
simo <svm.zhang@gmail.com>
2024-11-21 12:36:03 -0800

    initial commit; tests for BamArrays class

---
+++ tests/test_bamarray.py
@@ -0,0 +1,117 @@
+import sys
+from dataclasses import fields
+
+import numpy as np
+import polars as pl
+import pytest
+
+from tinyscibio import BamArrays
+from tinyscibio.bam import _MAX_CHUNK_SIZE
+
+
+@pytest.fixture
+def default_fields():
+    return [
+        "rnames",
+        "rends",
+        "rstarts",
+        "mqs",
+        "propers",
+        "primarys",
+        "sc_bps",
+    ]
+
+
+@pytest.fixture
+def optional_fields():
+    return ["qnames", "mm_ecnt", "indel_ecnt", "bqs", "mds"]
+
+
+@pytest.fixture
+def chunk_size(request):
+    return request.param
+
+
+@pytest.fixture
+def default_bamarray_df(request):
+    chunk_size = request.param
+    return pl.DataFrame(
+        {
+            "rnames": np.zeros(chunk_size, dtype=np.uint16),
+            "rstarts": np.zeros(chunk_size, dtype=np.int32),
+            "rends": np.zeros(chunk_size, dtype=np.int32),
+            "mqs": np.zeros(chunk_size, dtype=np.uint8),
+            "propers": np.empty(chunk_size, dtype=bool),
+            "primarys": np.empty(chunk_size, dtype=bool),
+            "sc_bps": np.zeros(chunk_size, dtype=np.int16),
+        }
+    )
+
+
+def test_bamarray_create_with_default(default_fields, optional_fields):
+    chunk_size = 10
+    bamarray = BamArrays.create(chunk_size)
+    for f in default_fields:
+        assert bamarray.__dict__[f].size == chunk_size
+
+    for f in optional_fields:
+        assert bamarray.__dict__[f].size == 0
+
+
+def test_bamarray_create_with_optional(optional_fields):
+    chunk_size = 10
+    bamarray = BamArrays.create(
+        chunk_size, with_ecnt=True, with_bq=True, with_md=True, with_qname=True
+    )
+
+    for f in optional_fields:
+        assert bamarray.__dict__[f].size == chunk_size
+
+    bamarray = BamArrays.create(chunk_size, with_ecnt=True)
+    assert bamarray.mm_ecnt.size == chunk_size
+    assert bamarray.indel_ecnt.size == chunk_size
+
+
+def test_bamarray_create_with_zero_chunk_size():
+    with pytest.raises(ValueError):
+        BamArrays.create(chunk_size=0)
+
+
+def test_bamarray_create_with_chunk_size_larger_than_max(default_fields):
+    chunk_size = _MAX_CHUNK_SIZE + 1
+    bamarray = BamArrays.create(chunk_size)
+    for f in default_fields:
+        assert bamarray.__dict__[f].size == _MAX_CHUNK_SIZE
+
+
+@pytest.mark.parametrize(
+    "default_bamarray_df, chunk_size",
+    [(1, 1), (10, 10), (100, 100)],
+    indirect=True,
+)
+def test_bamarray_to_df_without_content(default_bamarray_df, chunk_size):
+    bamarray = BamArrays.create(chunk_size)
+    assert bamarray.df(idx=chunk_size).shape[0] == chunk_size
+    assert bamarray.df(idx=chunk_size).equals(default_bamarray_df)
+
+
+@pytest.mark.parametrize(
+    "chunk_size",
+    [1, 10, 100],
+    indirect=True,
+)
+def test_bamarray_to_df_with_negative_idx(chunk_size):
+    with pytest.raises(ValueError):
+        bamarray = BamArrays.create(chunk_size)
+        bamarray.df(idx=-2)
+
+
+@pytest.mark.parametrize(
+    "chunk_size",
+    [1, 10, 100],
+    indirect=True,
+)
+def test_bamarray_to_df_with_idx_larger_than_alloc(chunk_size):
+    with pytest.raises(IndexError):
+        bamarray = BamArrays.create(chunk_size)
+        bamarray.df(chunk_size + 1)
